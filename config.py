b52afa6584172831004bce793b647b82badf718d
Mekan777-alt <mekan2678@gmail.com>
2024-01-15 16:53:49 +0300

    testong

--- config.py
+++ config.py
@@ -16,7 +16,8 @@ admin_user_id = os.getenv("ADMIN_ID")
 api_id = os.getenv("API_ID")
 hash_id = os.getenv("HASH_ID")
 
-client = TelegramClient('my_account_session', int(api_id), hash_id, flood_sleep_threshold=0)
+client = TelegramClient('my_account_session', int(api_id), hash_id,
+                        system_version="4.16.30-vxCUSTOM", device_model="Iphone14", app_version="1.0")
 
 bot = Bot(token=os.getenv("BOT_TOKEN"), parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot, storage=storage)
